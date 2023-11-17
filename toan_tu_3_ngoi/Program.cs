32d126ff16f7db67d02c506839c451de42d6fe5a
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-11-17 16:13:43 +0700

    c# can ban

---
+++ toan_tu_3_ngoi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace toan_tu_3_ngoi
+{
+    internal class Program
+    {
+        // nhập vào 1 điểm số bất kì và cho biết điểm đó có được học sing giỏi ko
+        static void Main(string[] args)
+        {
+            bool hocsinhkogioi = false;
+            Console.WriteLine("Nhập vào số điểm của bạn: ");
+            double n = double.Parse(Console.ReadLine());
+            if ( n <= 8.0)
+            {
+                hocsinhkogioi = true;
+            }
+            Console.WriteLine(hocsinhkogioi ?  "Bạn chưa là học sinh giỏi" : "Bạn là học sinh giỏi");
+            Console.ReadLine();
+        }
+    }
+}
