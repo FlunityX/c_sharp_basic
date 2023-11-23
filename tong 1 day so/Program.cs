d97dd2da6b7c34ad7cc83b4fe41a13341ca2679c
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-11-23 22:33:38 +0700

    vòng lặp for

---
+++ tong 1 day so/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace tong_1_day_so
+{
+    internal class Program
+    {
+        // tính tổng của A = 1+ 1/2+ 1/3+1/4+1/5 +... 1/N với N nhap tu ban phím
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap n :");
+            int N = int.Parse(Console.ReadLine());
+            double tong = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                tong += 1.0 / i;
+            }
+            Console.WriteLine(" tong cua bieu thuc a la :{0} ", tong);
+            Console.ReadLine();
+
+        }
+    }
+}
