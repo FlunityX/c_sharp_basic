868b509ef36dfb1de7b9310c359efb65644becc1
FlunityX <149858650+FlunityX@users.noreply.github.com>
2023-11-30 19:41:13 +0700

    region và endregion

---
+++ region_and_endregion/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace region_and_endregion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region lời chào 
+            Console.WriteLine("Hello, World!");
+            #endregion
+            #region xuất ra số đã nhập 
+            Console.WriteLine(" NHAP SO CUAR BAN :");
+            int N = int.Parse(Console.ReadLine());
+            Console.WriteLine(" SO BAN VUA NHAP LA " + N);
+            #endregion
+            Console.ReadLine();
+        }
+    }
+}
