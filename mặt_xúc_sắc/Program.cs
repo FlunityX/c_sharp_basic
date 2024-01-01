8c9a517f303b1c0a76ecfc61b557d4a963295638
FlunityX <149858650+FlunityX@users.noreply.github.com>
2024-01-01 16:36:57 +0700

    tối giản phân số

---
+++ mặt_xúc_sắc/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace mat_xuc_sac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập mặt xúc xắc nhảy lần 1:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập mặt xúc xắc nhảy lần 2:");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập mặt xúc xắc nhảy lần 3:");
+            int c = int.Parse(Console.ReadLine());
+
+            int matA = MatConLaiXucSac(a);
+            int matB = MatConLaiXucSac(b);
+            int matC = MatConLaiXucSac(c);
+            int tongMatConLai = TongMat(matA, matB, matC);
+            Console.WriteLine("Tổng số mặt con lại sau 3 lần gieo: " + tongMatConLai);
+            Console.ReadLine();
+        }
+
+        static int MatConLaiXucSac(int xucSac)
+        {
+            if (xucSac == 1)
+                return 6;
+            if (xucSac == 2)
+                return 5;
+            if (xucSac == 3)
+                return 4;
+            if (xucSac == 4)
+                return 3;
+            if (xucSac == 5)
+                return 2;
+            return 1;
+        }
+
+        static int TongMat(int matA, int matB, int matC)
+        {
+            return matA + matB + matC;
+        }
+    }
+}
---
+++ so_thảy_xúc_sắc/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace so_thảy_xúc_sắc
+{
+    internal class Program
+    {
+        // bạn thắng và hùng thi đọ số xúc xắc viết chương trình xem ai thắng 
+        // hùng thắng chữ xanh dương thắng thắng chữ xanh lá , hoà chữ màu vàng
+        static void Main(string[] args)
+        {
+            int random1 = new Random().Next(1,6);
+            int random2 = new Random().Next(1,6);
+            Console.WriteLine("hung tung duoc so xuc sac la :" +random1);
+            Console.WriteLine("thang tung do so xuc sac la :" + random2);
+            if (random1 > random2)
+            {
+                Console.ForegroundColor= ConsoleColor.Blue;
+                int chenhlenh = random1 - random2;
+                Console.WriteLine("hung da danh chien thang vi hon thang tan :" + chenhlenh);
+                Console.ResetColor();
+            }
+            else if(random1 == random2)
+            {
+                Console.ForegroundColor= ConsoleColor.Yellow;
+                Console.WriteLine("2 ban hoa nhau , ko ai thang ");
+                Console.ResetColor();
+            }
+            else{
+                Console.ForegroundColor = ConsoleColor.Green;
+                int chenhlenh1 = random2 - random1;
+                Console.WriteLine("thang da danh chien thang vi hon hung tan :" + chenhlenh1);
+                Console.ResetColor();
+            }
+            Console.ReadLine();
+        }
+    }
+}
