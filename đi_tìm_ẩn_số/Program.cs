f50b4f674c5f6119132fa0a1be3138223ca6ec2e
FlunityX <149858650+FlunityX@users.noreply.github.com>
2024-01-07 00:05:20 +0700

    so thảy xúc xắc

---
+++ đi_tìm_ẩn_số/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace đi_tìm_ẩn_số
+{
+    // tìm số x biết : 1+2+3+...+x<=N với N là giá trị đã nhập 
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("nhap n :");
+            int n=int.Parse(Console.ReadLine());
+            int s = sum(n);
+            Console.WriteLine("gia tri cua x la :"+s);
+            Console.ReadLine();
+        }
+        static int sum(int n)
+        {
+            int s = 0;
+            int x = 1;
+            while (s < n)
+            {
+                x++;
+                s += x;
+            }
+            return x - 1;
+        }
+    }
+}
