e8a991d45e0e3caad00a181ba6839eae82c52c60
FlunityX <149858650+FlunityX@users.noreply.github.com>
2024-01-24 10:06:15 +0700

    try parse trong c#

---
+++ try_pasre/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace try_pasre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập một số nguyên n (nhỏ hơn 50):");
+            string input = Console.ReadLine();
+
+            int n;
+            // out là tham số đầu ra n là đầu ra thành công của input
+            //  && là toán tử và cho biết phải thoả mãn cả 2 mới out n , nếu ko thực hiện else và nhập lại 
+            if (int.TryParse(input, out n) && n < 50)
+            {
+                // Đầu vào hợp lệ và n nhỏ hơn 50
+                Console.WriteLine("Số n đã nhập: " + n);
+            }
+            else
+            {
+                // Đầu vào không hợp lệ hoặc n không nhỏ hơn 50
+                Console.WriteLine("Đầu vào không hợp lệ. Vui lòng nhập lại.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
