5e366809b59e489a221fee5090aea88eabfa2e06
nguyễn mạnh <manharia123@gmail.com>
2024-03-03 14:25:08 +0700

    key word is

---
+++ từ khoá is/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace từ_khoá_is
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 56;
+            // từ khoá is nhằm tránh lặp lại tên biến
+
+           if( result is > 40 and < 69)
+            {
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
