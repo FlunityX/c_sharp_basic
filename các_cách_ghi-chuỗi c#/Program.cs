3fc8a30e928d01dd4b18c9508816a94629a8a461
nguyễn mạnh <manharia123@gmail.com>
2024-03-03 14:33:22 +0700

    các cách ghi chuỗi trong c#

---
+++ các_cách_ghi-chuỗi c#/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace các_cách_ghi_chuỗi_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "manh";
+            int age = 20;
+            // cách 1:
+            Console.WriteLine("ten cua ban la :{0} va tuoi la :{1} ",name,age);
+            // cách 2 :
+            Console.WriteLine("ten cua ban la :" + name + " va tuoi la :" + age);
+            // cách 3 :
+            Console.WriteLine($"ten cua ban la :{name} va tuoi la :{age}");
+            Console.ReadLine();
+
+        }
+    }
+}
