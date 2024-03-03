08fb07e21677f34ce75013199de2ca920bfd6af0
nguyễn mạnh <manharia123@gmail.com>
2024-03-03 14:31:17 +0700

    tham số mặc định

---
+++ thamsomacdinh/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace thamsomacdinh
+{
+    internal class Program
+    {
+        static void method (string name = "jack")
+        {
+            Console.WriteLine(name);
+        } 
+        static void Main(string[] args)
+        {
+            method();
+            method("david");
+            Console.ReadLine();
+        }
+    }
+}
